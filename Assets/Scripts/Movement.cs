102e272cb78b7055e0acaee25f0bedbf38b0a66e
brennanm2 <brennan.moore27@pallottihs.info>
2026-05-21 13:58:45 -0400

    Revert "Animation"
    This reverts commit 34fbbf78cf5b95bf027c8618e42e4f38e459a46c.

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,7 +5,9 @@ public class Movement : MonoBehaviour
     [Header("Movement Settings")]
     public float forwardSpeed = 10f;
     public float jumpForce = 5f;
-    public Vector3 targetPosition = new Vector3(30f, 2.6f, -125.5f);
+    public Vector3 targetPosition = new Vector3(30f, 2.6f, -125.5f);\
+    public 
+
     public Rigidbody rb;
     public bool onGround;
 
@@ -31,11 +33,6 @@ public class Movement : MonoBehaviour
        {
         transform.position = transform.position + new Vector3(17, 0, 0);
        }
-
-        if(Input.GetKeyDown(KeyCode.S))
-        {
-            isSliding
-        }
 
     
     }
